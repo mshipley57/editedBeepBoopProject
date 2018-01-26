92875057d7e1de798b0430d0660da1d3c2b7b8d9
Mariah Shipley <mshipley57@gmail.com>
2018-01-26 14:54:03 -0800

    fix js and add logics

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,38 @@
-$(document).ready(function(){
+//back end logic
+function beepBoop(userInput) {
+    var numbersArray = [];
+    for (var i = 1; i <= userInput; i++) {
+        var iString = i.toString();
+
+        if (i % 3 === 0) {
+            numbersArray.push("I'm sorry Dave, I'm afraid I can't do that!");
+        } else if (iString.includes("1")) {
+            numbersArray.push("<br>boop!</br>");
+        } else if (iString.includes("0")) {
+            numbersArray.push("<br>beep!</br>");
+        } else {
+            numbersArray.push("<br>" + i + "</br>");
+
+        }
+    }
+    return numbersArray
+}
+
+//ui logic
+$(document).ready(function() {
+
+
 
     $(".beepForm").submit(function(event) {
-    event.preventDefault();
+        var userInput = $(".numberInput").val();
+        console.log(userInput)
 
-    var userInput = $(".sentence").val();
+        event.preventDefault();
+        var displayThisArray = beepBoop(userInput)
 
-        $(".appendToMe").html("");
+        $(".appendToMe").html(displayThisArray);
+    });
 
 
-      for (var i = 1; i <= userInput; i ++) {
-        var iString = i.toString()
 
-        if ( i % 3 == 0)
-        {
-          $(".appendToMe").append("I'm sorry Dave, I'm afraid I can't do that!")
-        }else if (iString.includes("1")){
-          $(".appendToMe").append("<br>boop!</br>")
-        }else if (iString.includes("0")){
-          $(".appendToMe").append("<br>beep!</br>")
-        }else
-        {
-          $(".appendToMe").append("<br>"+i+"</br>")
-        }
-      }
-
-   });
 });
